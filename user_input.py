98fe5b940990ccb20ea15237dd15ea02187a95eb
Enyewashebr <Enyewashebir0@gmail.com>
2024-08-24 15:28:01 +0300

    Introduction to python!

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input('what is your name? ')
+age = int(input('how old are you? '))
+location = input('where are you live?  ')
+print(f'hello {name}, You are {age} years old and you live in {location}.')
