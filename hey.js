dec6a3a946c6b60fb7355f78f39689056bd5e10d
Enyewashebr <Enyewashebir0@gmail.com>
2025-03-12 13:58:01 +0300

    week-1 python assignment

---
+++ hey.js
@@ -0,0 +1,22 @@
+// const name = "Person";
+// const orderId = 12345;
+// const greeting = `Hello ${name}, your order ID is ${orderId}.`;
+// console.log(greeting);
+
+
+// function say(message) {
+//   console.log(message);
+// }
+// say('hello')
+// console.log(3+4)
+//rguments = [1, 2, 3, 4, 5];
+function add() {
+    
+  let sum = 0;
+  rguments = [1, 2, 3, 4, 5];
+  for (let i = 0; i < rguments.length; i++) {
+    sum += rguments[i];
+  }return sum;
+  
+}
+console.log(add(rguments)); // 15
--- user_input.py
+++ user_input.py
@@ -1,4 +1,7 @@
-name = input('what is your name? ')
-age = int(input('how old are you? '))
-location = input('where are you live?  ')
-print(f'hello {name}, You are {age} years old and you live in {location}.')
+# name = input('what is your name? ')
+# age = int(input('how old are you? '))
+# location = input('where are you live?  ')
+# print(f'hello {name}, You are {age} years old and you live in {location}.')
+# frv={1,2,3,4}
+# frv[4] = 6
+# console.(frv[4])
